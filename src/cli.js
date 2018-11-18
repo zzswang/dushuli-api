c08cc306ab6e458afe541e6ba75d60a7c9e4d2ed
zzswang <zzswang@gmail.com>
2018-11-18 16:16:33 +0800

    feat: qrcode

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,9 @@ import program from "commander";
 import fs from "fs-extra";
 import nanoid from "nanoid";
 import path from "path";
+import QRCode from "qrcode";
+import moment from "moment";
+import { generate } from "branded-qr-code";
 
 import pkg from "../package.json";
 
@@ -21,6 +24,39 @@ program
       writeStream.write("\r\n");
     }
     console.log("done");
+  });
+
+program
+  .command("qrcode")
+  .description("generate icode")
+  .action(async () => {
+    const start = moment("2019-01-01T12:12:12.000Z");
+    const end = moment("2020-01-01T12:12:12.000Z");
+    const logo = path.resolve(process.cwd(), "assets/logo.png");
+
+    for (let d = start; d < end; d.add(1, "days")) {
+      const name = d.format("YYYYMMDD");
+      const url = `https://yuewen365.com/${name}`;
+      const file = path.resolve(process.cwd(), `build/qrcode/${name}.png`);
+      const buf = await generate({ text: url, path: logo, ratio: 4 });
+      await fs.ensureFile(file);
+      await fs.writeFileSync(file, buf);
+
+      // QRCode.toFile(
+      //   file,
+      //   `https://yuewen365.com/${name}`,
+      //   {
+      //     color: {
+      //       dark: "#000", // Blue dots
+      //       light: "#0000", // Transparent background
+      //     },
+      //   },
+      //   function(err) {
+      //     if (err) throw err;
+      //     console.log("done");
+      //   }
+      // );
+    }
   });
 
 program.parse(process.argv);
